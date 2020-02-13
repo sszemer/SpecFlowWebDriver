726ff9275e77355d29999a5424acf51c54fdfd82
sszemer <Sk@te123#>
2020-02-13 09:12:00 +0100

    Dodaj pliki projektów.

---
+++ SpecFlowWebDriver/WikiSearchSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SpecFlowWebDriver
+{
+    [Binding]
+    public class WikiSearchSteps
+    {
+
+        static IWebDriver driver;
+
+        [BeforeFeature]
+        static void setUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Given(@"Wiki page is open")]
+        public void GivenWikiPageIsOpen()
+        {
+            driver.Url = "https://pl.wikipedia.org/";
+        }
+        
+        [When(@"I search for a (.*)")]
+        public void WhenISearchForA(string p0)
+        {
+            IWebElement e = driver.FindElement(By.Id("searchInput"));
+            e.SendKeys(p0);
+            e.Submit();
+        }
+        
+        [Then(@"The definition of (.*) is displayed")]
+        public void ThenTheDefinitionOfIsDisplayed(string p0)
+        {
+            driver.Title.Equals(p0);
+        }
+
+        [AfterFeature]
+        static void tearDown()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SpecFlowWebDriver/wikiSearch.feature
@@ -0,0 +1,15 @@
+﻿Feature: wikiSearch
+	In order to know what things are
+	As an idiot
+	I want to search for the definitions
+
+@searchDefinitions
+Scenario: Find definitions
+	Given Wiki page is open
+	When I search for a <definition>
+	Then The definition of <definition> is displayed
+
+	Examples: 
+	| definition |
+	| apple      |
+	| pear       |
