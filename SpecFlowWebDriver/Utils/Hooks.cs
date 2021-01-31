7694c2033573d420b46a908774eafdd3a5dccfdc
sszemer <sszemer@gmail.com>
2021-01-31 20:43:24 +0100

    turned parallelism on prepared for extent report to support parallel test execution

--- SpecFlowWebDriver/Utils/Hooks.cs
+++ SpecFlowWebDriver/Utils/Hooks.cs
@@ -14,26 +14,32 @@ namespace SpecFlowWebDriver.Utils
     public sealed class Hooks
     {
 
+        private static ReportPOCO reportPOCO;
+
         [BeforeTestRun]
         public static void BeforeTestRun()
         {
-            EnvironmentHelper.EnvironmentType = EnvironmentType.LAMBDA_TEST;
+            EnvironmentHelper.EnvironmentType = EnvironmentType.LOCAL;
             Reporter.SetupExtentReports();
+            reportPOCO = new ReportPOCO();
         }
 
         [BeforeFeature]
         public static void BeforeFeature(FeatureContext featureContext)
         {
+            reportPOCO.Features.Add(new FeaturePOCO(featureContext.FeatureInfo.Title, featureContext.FeatureInfo.Description));
             Reporter.Feature = Reporter.Report.CreateTest<Feature>(featureContext.FeatureInfo.Title, featureContext.FeatureInfo.Description);
         }
 
         [BeforeScenario]
-        public static void BeforeScenario(ScenarioContext scenarioContext)
+        public static void BeforeScenario(ScenarioContext scenarioContext, FeatureContext featureContext)
         {
             if (scenarioContext.ScenarioInfo.Tags.Contains("web")) scenarioContext.Set<DriverType>(DriverType.Web);
             if (scenarioContext.ScenarioInfo.Tags.Contains("mobile")) scenarioContext.Set<DriverType>(DriverType.Mobile);
             if (scenarioContext.ScenarioInfo.Tags.Contains("desktop")) scenarioContext.Set<DriverType>(DriverType.Desktop);
             if (scenarioContext.ScenarioInfo.Tags.Contains("nodriver")) scenarioContext.Set<DriverType>(DriverType.None);
+            reportPOCO.Features.Find(feature => feature.Title.Equals(featureContext.FeatureInfo.Title))
+                .Scenarios.Add(new ScenarioPOCO(scenarioContext.ScenarioInfo.Title));
             Reporter.Scenario = Reporter.Feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
             try
             {
@@ -41,31 +47,47 @@ namespace SpecFlowWebDriver.Utils
             }
             catch (Exception e)
             {
+                reportPOCO.Features.Find(feature => feature.Title.Equals(featureContext.FeatureInfo.Title))
+                    .Scenarios.Find(scenario => scenario.Title.Equals(scenarioContext.ScenarioInfo.Title))
+                    .Steps.Add(new StepPOCO($"scenario failed: {e.Message}", StepDefinitionType.Given, Status.Error));
                 Reporter.Scenario.CreateNode<Given>($"scenario failed: {e.Message}").Fail("").Log(Status.Error, e);
                 Assert.Ignore($"scenario failed: {e.Message}");
             }
         }
 
         [BeforeStep]
-        public static void BeforeStep(ScenarioContext scenarioContext)
+        public static void BeforeStep(ScenarioContext scenarioContext, FeatureContext featureContext)
         {
             switch (scenarioContext.StepContext.StepInfo.StepDefinitionType)
             {
                 case StepDefinitionType.Given:
                     Reporter.Step = Reporter.Scenario.CreateNode<Given>(scenarioContext.StepContext.StepInfo.Text);
+                    reportPOCO.Features.Find(feature => feature.Title.Equals(featureContext.FeatureInfo.Title))
+                        .Scenarios.Find(scenario => scenario.Title.Equals(scenarioContext.ScenarioInfo.Title))
+                        .Steps.Add(new StepPOCO(scenarioContext.StepContext.StepInfo.Text, StepDefinitionType.Given));
                     break;
                 case StepDefinitionType.When:
                     Reporter.Step = Reporter.Scenario.CreateNode<When>(scenarioContext.StepContext.StepInfo.Text);
+                    reportPOCO.Features.Find(feature => feature.Title.Equals(featureContext.FeatureInfo.Title))
+                        .Scenarios.Find(scenario => scenario.Title.Equals(scenarioContext.ScenarioInfo.Title))
+                        .Steps.Add(new StepPOCO(scenarioContext.StepContext.StepInfo.Text, StepDefinitionType.When));
                     break;
                 case StepDefinitionType.Then:
                     Reporter.Step = Reporter.Scenario.CreateNode<Then>(scenarioContext.StepContext.StepInfo.Text);
+                    reportPOCO.Features.Find(feature => feature.Title.Equals(featureContext.FeatureInfo.Title))
+                        .Scenarios.Find(scenario => scenario.Title.Equals(scenarioContext.ScenarioInfo.Title))
+                        .Steps.Add(new StepPOCO(scenarioContext.StepContext.StepInfo.Text, StepDefinitionType.Then));
                     break;
             }
         }
 
         [AfterStep]
-        public static void AfterStep(ScenarioContext scenarioContext)
+        public static void AfterStep(ScenarioContext scenarioContext, FeatureContext featureContext)
         {
+            string screenshot = scenarioContext.Get<DriverType>() is not DriverType.None ? DriverProvider.GetScreenshot(scenarioContext) : string.Empty;
+            var step = reportPOCO.Features.Find(feature => feature.Title.Equals(featureContext.FeatureInfo.Title))
+                .Scenarios.Find(scenario => scenario.Title.Equals(scenarioContext.ScenarioInfo.Title))
+                .Steps.Find(step => step.Title.Equals(scenarioContext.StepContext.StepInfo.Text));
             if (scenarioContext.TestError != null)
             {
                 string url = DriverProvider.GetUrl(scenarioContext);
@@ -78,9 +100,12 @@ namespace SpecFlowWebDriver.Utils
                     { "PageSource", $"<a href=\"{pageSource}\">{pageSource}</a>"}
                 };
                 Reporter.Step.Fail(MarkupHelper.CreateTable(data));
+                step.PageSource = pageSource;
+                step.URL = url;
             }
+            step.Screenshot = screenshot;
             if (scenarioContext.Get<DriverType>() is not DriverType.None)
-                Reporter.Step.Log(Status.Info, MediaEntityBuilder.CreateScreenCaptureFromPath(DriverProvider.GetScreenshot(scenarioContext)).Build());
+                Reporter.Step.Log(Status.Info, MediaEntityBuilder.CreateScreenCaptureFromPath(screenshot).Build());
         }
 
         [AfterScenario]
@@ -101,6 +126,9 @@ namespace SpecFlowWebDriver.Utils
         public static void AfterTestRun()
         {
             Reporter.Report.Flush();
+            foreach (var feature in reportPOCO.Features) {
+            
+            }
         }
     }
 }
---
+++ SpecFlowWebDriver/Utils/ReportPOCO.cs
@@ -0,0 +1,59 @@
+﻿using AventStack.ExtentReports;
+using System.Collections.Generic;
+using TechTalk.SpecFlow.Bindings;
+
+namespace SpecFlowWebDriver.Utils
+{
+    public class ReportPOCO
+    {
+        public ReportPOCO()
+        {
+            Features = new List<FeaturePOCO>();
+        }
+        public List<FeaturePOCO> Features { get; }
+    }
+
+    public class FeaturePOCO
+    {
+        public FeaturePOCO(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            Scenarios = new List<ScenarioPOCO>();
+        }
+        public string Title { get; }
+        public string Description { get; }
+        public List<ScenarioPOCO> Scenarios { get; }
+    }
+    public class ScenarioPOCO
+    {
+        public ScenarioPOCO(string title)
+        {
+            Title = title;
+            Steps = new List<StepPOCO>();
+        }
+        public string Title { get; }
+        public List<StepPOCO> Steps { get; }
+
+    }
+    public class StepPOCO
+    {
+        public StepPOCO(string title, StepDefinitionType stepType)
+        {
+            Title = title;
+            StepType = stepType;
+        }
+        public StepPOCO(string title, StepDefinitionType stepType, Status stepStatus)
+        {
+            Title = title;
+            StepType = stepType;
+            StepStatus = stepStatus;
+        }
+        public string Title { get; }
+        public StepDefinitionType StepType { get; }
+        public Status StepStatus { get; set; }
+        public string URL { get; set; }
+        public string PageSource { get; set; }
+        public string Screenshot { get; set; }
+    }
+}
