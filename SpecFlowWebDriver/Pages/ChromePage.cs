9ce5f607f218f534c7da0ee286a6a294c73276c2
sszemer <sszemer@gmail.com>
2021-02-01 22:23:18 +0100

    improved android steps

--- SpecFlowWebDriver/Pages/ChromePage.cs
+++ SpecFlowWebDriver/Pages/ChromePage.cs
@@ -1,31 +1,27 @@
 ﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Appium;
-using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.UI;
-using System;
+using System.Threading;
 
 namespace SpecFlowWebDriver.Pages
 {
     public class ChromePage : CommonPage
     {
         public ChromePage(RemoteWebDriver driver) : base(driver) { }
-        public IWebElement SearchInput => driver.FindElement(By.Id("com.android.chrome:id/search_box_text"));
-        public IWebElement AcceptTerms => driver.FindElement(By.Id("com.android.chrome:id/terms_accept"));
-        public IWebElement NoThanks => driver.FindElement(By.Id("com.android.chrome:id/negative_button"));
-        public IWebElement UrlBar => driver.FindElement(By.Id("com.android.chrome:id/url_bar"));
-        public IWebElement KeyboardReturn => driver.FindElement(By.Id("com.android.chrome:id/url_bar"));
+        public IWebElement SearchInput => driver.FindElement(By.XPath("//android.widget.EditText[@resource-id='com.android.chrome:id/search_box_text']"));
+        public IWebElement AcceptTerms => driver.FindElement(By.XPath("//android.widget.Button[@resource-id='com.android.chrome:id/terms_accept']"));
+        public IWebElement NoThanks => driver.FindElement(By.XPath("//android.widget.Button[@resource-id='com.android.chrome:id/negative_button']"));
+        public IWebElement UrlBar => driver.FindElement(By.XPath("//android.widget.EditText[@resource-id='com.android.chrome:id/url_bar']"));
         public IWebElement GoogleInput => driver.FindElement(By.ClassName("android.widget.EditText"));
+        public IWebElement FirstSearchHint => driver.FindElement(By.XPath("//android.widget.TextView[@resource-id='com.android.chrome:id/line_1']"));
         public void GoogleThing(string thing)
         {
             AcceptTerms.Click();
             NoThanks.Click();
-            var wait = new WebDriverWait(driver,TimeSpan.FromSeconds(30));
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("com.android.chrome:id/search_box_text")));
+            Thread.Sleep(5000);
             SearchInput.Click();
             UrlBar.Clear();
             UrlBar.SendKeys(thing);
-            ((AndroidDriver<AppiumWebElement>)driver).PressKeyCode(AndroidKeyCode.Enter);
+            FirstSearchHint.Click();
         }
     }
 }
